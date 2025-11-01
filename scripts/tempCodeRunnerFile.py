870c925930bd29aabae95c824a4c03d0888cc200
hakkipyrz <hakkipyrz@gmail.com>
2025-11-01 13:46:05 +0300

    Remove __pycache__ and tempCodeRunnerFile.py

--- scripts/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-import csv
-with open('data/Linux_2k.log_structured.csv', mode ='r')as file:
-    data_csv = csv.reader(file)
-
-    for line in data_csv:
-        logs= str(line)
-        logs_split= logs.split()
-        event_id= logs_split[0]
-        print(event_id)
